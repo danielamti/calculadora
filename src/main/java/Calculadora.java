7c2369dcdbfbf68fef17526844aec75a87e6a8e3
danielamti <130715149+danielamti@users.noreply.github.com>
2024-03-14 00:34:18 +0000

    meu primeiro commit criacao

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,15 @@
+class Calculadora{
+  public void multiplicacao(String[] args) {
+    int numero1;
+    int numero2;
+    int resultado;
+
+    numero1 = 5;
+    numero2 = 2;
+    resultado = num1, * num2;
+    System.out.println(resultado);
+
+    
+
+  }
+}g
---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import sun.awt.www.content.audio.x_aiff;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+
+  
+  public static void main(String[] args) {
+  scanner ler = new scanner(System.in);
+    int numero1;
+    int numero2;
+    int resultado;
+
+    
+    numero1 = 5;
+    numero2 = 2;
+  
+  resultado = ("numero1", * "numero2");
+    System.out.println("resultado");
+  
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
